2989181a11d88f7a72f0ae8531fcee627783349a
yura212137 <104753546+yura212137@users.noreply.github.com>
2022-05-10 21:35:35 +0900

    initial commit

--- front-page.php
+++ front-page.php
@@ -1,52 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
 
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- サイトのタイトル -->
-    <title>Hamburger</title>
-    <!-- サイトの説明文 -->
-    <meta name="description" content="ハンバーガーを売りにしたオシャレなカフェデモサイト">
-    <!-- サイトのアイコン -->
-    <link rel="shortcut icon" href="#" type="image/x-icon" />
-    <!-- reset.css destyle -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
-    <!-- google fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@400;700&family=Roboto:wght@400;700&display=swap"
-      rel="stylesheet">
-    <link rel="stylesheet" href="./asset/css/style.min.css">
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/48595fcda8.js" crossorigin="anonymous"></script>
-  </head>
-
-  <body>
-    <!-- .l-header -->
-    <div class="l-main">
-      <!-- hamburger button -->
-      <input id="menu" type="checkbox">
-      <label for="menu" class="c-button open">Menu</label>
-      <label for="menu" class="back"></label>
-      <!-- hamburger button -->
-      <div class="l-main__headermain">
-        <header class="l-header">
-
-          <div class="l-header__title">
-            Hamburger
-          </div>
-
-          <form class="l-header__searchform c-searchform" action="#" method="get">
-            <input class="c-searchform__input" type="text" name="search" placeholder="&#xf002">
-            <input class="c-searchform__button" type="submit" name="submit" value="検索">
-          </form>
-
-
-        </header>
-        <!-- .l-header -->
-
+<?php get_header() ;?>
         <!-- main -->
         <main>
           <!-- .p-fv -->
@@ -116,91 +69,11 @@
                 </div>
             </div>
           </section>
-          <!-- .p-access -->
+          <!-- .p-access -->  
         </main>
       </div>
       <!-- main -->
-      <!-- aside -->
-      <aside class="l-aside">
-        <label for="menu" class="close">×</label>
-        <div class="l-aside__menu">
-          <a href="">Menu</a>
-        </div>
-        <ul class="l-aside__burger">
-          <li><a href="">バーガー</a></li>
-          <ul>
-            <li><a href="">ハンバーガー</a></li>
-            <li><a href="">チーズバーガー</a></li>
-            <li><a href="">テリヤキバーガー</a></li>
-            <li><a href="">アボカドバーガー</a></li>
-            <li><a href="">フィッシュバーガー</a></li>
-            <li><a href="">ベーコンバーガー</a></li>
-            <li><a href="">チキンバーガー</a></li>
-          </ul>
-        </ul>
-        <ul class="l-aside__side">
-          <li><a href="">サイド</a></li>
-          <ul>
-            <li><a href="">ポテト</a></li>
-            <li><a href="">サラダ</a></li>
-            <li><a href="">ナゲット</a></li>
-            <li><a href="">コーン</a></li>
-          </ul>
-        </ul>
-        <ul class="drink">
-          <li><a href="">ドリンク</a></li>
-          <ul>
-            <li><a href="">コーラ</a></li>
-            <li><a href="">ファンタ</a></li>
-            <li><a href="">オレンジ</a></li>
-            <li><a href="">アップル</a></li>
-            <li><a href="">紅茶（Ice/Hot）</a></li>
-            <li><a href="">コーヒー（Ice/Hot）</a></li>
-          </ul>
-        </ul>
-      </aside>
-    </div>
-    <!-- aside -->
+<?php get_sidebar() ;?>
+<?php get_footer() ;?>
 
-    <!-- l-footer -->
-    <footer class="l-footer">
-      <div class="l-footer__link">
-      <a href="" class="l-footer__shop">
-        ショップ情報
-      </a>
-      <span>|</span>
-      <a href="" class="l-footer__history">
-        ヒストリー
-      </a>
-    </div>
-      <small>Copyright: RaiseTech</small>
-    </footer>
 
-    <!-- .l-footer -->
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-    <script>
-      $(function () {
-        $(".c-button").on("click", function () {
-          $("body").toggleClass("open");
-          $(this).toggleClass("open");
-          $(window).resize(function () {  //リサイズされたときの処理
-            $(".open").removeClass("open");
-            $('#menu').prop('checked', false);
-          });
-        });
-        $(".close").on("click", function () {
-          $("body").removeClass("open");
-        });
-        $(".back").on("click", function () {
-          $("body").removeClass("open");
-        });
-      });
-    </script>
-    <!-- jQuery -->
-  </body>
-
-</html>
